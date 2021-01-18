384a0b4d3e5c0ac7c7cbca7ee7f098168b690644
tiewangw <tiewanga@126.com>
2021-01-18 19:32:31 +0800

    位运算

---
+++ src/demo12-bitOpt/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+
+// 位运算
+	fmt.Println(2&3) 	//2
+	fmt.Println(2|3)	//3
+	fmt.Println(2^3)	//1
+	fmt.Println(-2^2)	//-4
+// 位移运算
+	fmt.Println(1<<2)	//4
+	fmt.Println(1>>2)	//0
+}
